0a6fb75451c3e956938e2565eaee12997c3f280b
milka77 <55999046+milka77@users.noreply.github.com>
2021-07-16 12:30:50 +0100

    Added style to news and listed them in the pages/index view

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -5,6 +5,20 @@
       <p class="lead">Simple Home App built with PHP milkaMVC Framework</p>
     </div>
   </div>
+  <!-- News list -->
   <h2 class="text-center">NEWS</h2>
-  <hr>
+
+
+  <?php 
+    foreach($data['news'] as $news) { ?>
+      <hr class="mx-auto my-4 w-75">
+      <div class="mx-auto news w-75">
+        <h4 class="text-center mb-3"><?php echo $news->title; ?></h4>
+        <p class="mb-3"><?php echo $news->body; ?> </p>
+        <small class="small">Added by: <strong><?php echo $news->added_by; ?></strong> at: <strong><?php echo $news->date; ?></strong></small>
+      </div>
+      
+  <?php } ?>
+
+  <!-- End of News list -->
 <?php require APPROOT . '/views/inc/footer.php'; ?>
