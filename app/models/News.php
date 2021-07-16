e9c4efbff9bf87cca58f36aefd4884d3c033304d
milka77 <55999046+milka77@users.noreply.github.com>
2021-07-16 10:42:12 +0100

    Added addNews logic to base News model

--- app/models/News.php
+++ app/models/News.php
@@ -6,4 +6,21 @@
       $this->db = new Database;
     }
 
+    // Add News to DB
+    public function addNews($data) {
+      // Query
+      $this->db->query('INSERT INTO news (title, body, added_by) VALUES(:title, :body, :added_by');
+
+      // Bind Values
+      $this->db->bind(':title', $data['title']);
+      $this->db->bind(':body', $data['body']);
+      $this->db->bind(':added_by', $SESSION['user_first_name']);
+
+      // Execute
+      if($this->db->execute()) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
