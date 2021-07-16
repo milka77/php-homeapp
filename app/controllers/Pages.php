a4d9c19eeb716f2567ec36fb5f5260aef18cee56
milka77 <55999046+milka77@users.noreply.github.com>
2021-07-16 11:16:54 +0100

    moved news functions to pages controller and removed News contoller file

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,12 +2,17 @@
 
 class Pages extends Controller {
   public function __construct(){
-
+    $this->newsModel = $this->model('News');
+    $this->userModel = $this->model('User');
   }
 
   public function index(){
+    // Get all the news from the DB
+    $news = $this->newsModel->getNews();
+
     $data = [
       'title' => 'Milka Home App',
+      'news' => $news,
     ];
 
     $this->view('pages/index', $data);
